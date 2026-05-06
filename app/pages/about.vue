9ea791fcd3163bdae5451af9e3ea22c5b0d8608e
ELAND\zhongqilin <z0925955648@gmail.com>
2026-05-06 17:38:37 +0800

    build css & pages

---
+++ app/pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="font-black text-2xl">About</h1>
+  <NuxtLink to="/">Home</NuxtLink>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: "My Awsome Website!"
+})
+</script>
